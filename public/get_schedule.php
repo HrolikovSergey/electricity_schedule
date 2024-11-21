471b63a48345d45e678b37408b1b6e35f2068978
Khrolikov Serhii <sergey@onix-systems.com>
2024-11-21 17:42:28 +0200

    Updated data gathering url

--- public/get_schedule.php
+++ public/get_schedule.php
@@ -2,7 +2,7 @@
 
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
 if($id) {
-    $url = "https://kiroe.com.ua/electricity-blackout/websearch/$id?ajax=1";
+    $url = "https://kiroe.com.ua/electricity-blackout/websearch/v2/$id?ajax=1";
     if($response = file_get_contents($url)) {
         echo $response;
         exit;
